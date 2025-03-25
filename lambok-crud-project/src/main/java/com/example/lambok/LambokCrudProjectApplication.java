22237f479e28995fee1f7aabf812a60b4838a434
Dhanaji Jodhale <dhanajijondhale95@gmail.com>
2025-03-25 11:58:37 +0530

    Spring CRUD with Lambok

---
+++ lambok-crud-project/src/main/java/com/example/lambok/LambokCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lambok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambokCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambokCrudProjectApplication.class, args);
+	}
+
+}
---
+++ lambok-crud-project/src/test/java/com/example/lambok/LambokCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lambok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambokCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
